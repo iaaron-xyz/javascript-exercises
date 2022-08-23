8614bb3824f6792676239417121b666322d1fd49
Aaron <iaaron.xyz@outlook.com>
2022-08-23 02:50:20 -0500

    complete: 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(originalString, repetitions) {
+    // Only positive numbers allowed
+    if (repetitions >= 0) {
+        // Concatenate the same string num times
+        let repeatedString = "";
+        for (i=0; i < repetitions; i++) {
+            repeatedString += originalString;
+        }
+        return repeatedString;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
