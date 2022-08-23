dea9471cd141d11028d3b17de9a6c463c51abbeb
Aaron <iaaron.xyz@outlook.com>
2022-08-23 03:13:36 -0500

    complete: 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+function reverseString(string) {
+    const lengthString = string.length;
+    const stringArray = string.split("");
+    // Invert the text
+    let invertedArray = [];
+    for (i=lengthString-1; i >= 0; i--) {
+        invertedArray.push(stringArray[i]);
+    }
+    // return the inverted array as string
+    return invertedArray.join("");
 
 };
 
