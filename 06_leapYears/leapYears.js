6d8546c963a77d0b374921f71274d13b7bbe987f
Aaron <iaaron.xyz@outlook.com>
2022-08-24 03:29:38 -0500

    finished: 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // years not divisible by 4 are normal years
+  if (!(year % 4 == 0)) {
+    return false;
+  }
+  // years divisible by 4 but not by 100 are leap years
+  else if (!(year % 100 == 0)) {
+    return true;
+  }
+  // years divisible by 4 and 100 are common years ...
+  else if (!(year % 400 == 0))
+  {
+    return false;
+  }
+  // ... unless divisible by 400, then is a leap year
+  else {
+    return true;
+  }
 };
+
+// Basic test
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
