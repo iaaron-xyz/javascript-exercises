c39c6280500ae7767d464b752a41b1526babd7ad
Aaron <iaaron.xyz@outlook.com>
2022-08-24 03:28:39 -0500

    finished: 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, item) {
+    // Create a new array wont contain removed elements
+    const lenOriginalArray = originalArray.length;
+    const lenArguments = arguments.length;
+    let newArray = [];
+    let remove;
+    // Copy items from old array to new array except listed ones
+    for (let i = 0; i < lenOriginalArray; i++) {
+        remove = false;
+        // do not add listed elements into the new array
+        for (let j = 1; j < lenArguments; j++) {
+            if (originalArray[i] === arguments[j]) {
+                remove = true; 
+                break;
+            }
+        }
+        // Add items not listed
+        if (!remove) {
+            newArray.push(originalArray[i]);
+        }
+    }
 
+    return newArray;
 };
+
+// Basic test
+console.log(removeFromArray([1, 1, "taco", "ho", "hey", 1], 1, 2, "hey", "foo"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
