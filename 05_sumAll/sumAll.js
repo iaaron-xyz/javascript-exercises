05f4dbb1e97df3d951bbbcc2b7a0380f8709dead
Aaron <iaaron.xyz@outlook.com>
2022-08-24 03:29:11 -0500

    finished: 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  
+  let total = 0;
+  let start;
+  let end;
+  
+  // both arguments must be numbers
+  if (!(typeof num1 == "number" && typeof num2 == "number")) {
+    return "ERROR";
+  }
+  // Both numbers must be positive integers
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+  }
 
+  // set start and end numbers
+  if (num2 > num1) {
+    start = num1;
+    end = num2;
+  }
+  else {
+    start = num2;
+    end = num1;
+  }
+
+  // Add all the integers between start and end (inclusive)
+  for (let i = start; i < end + 1; i++) {
+    total += i;
+  }
+
+  return total;
 };
+
+// basic test
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
