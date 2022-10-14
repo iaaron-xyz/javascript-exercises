662ee5647cb7820d0bf1667ac1b17ae64c587f08
Aaron <iaaron.xyz@outlook.com>
2022-10-14 12:54:40 -0500

    finished 09_palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Remove all non alphanumerical characters and set to lower case using RegEx
+    let newStr = str.replace(/,| |\.|!/g, "").toLowerCase();
+    // both extremes must coincide
+    const lenStr = newStr.length;
+    const halfLenStr = Math.round(lenStr/2);
+    for (let i = 0; i <= lenStr-1; i++) {
+        // characters in reflected positions must coincide
+        if (halfLenStr > i) {
+            if (newStr[i] == newStr[(lenStr-1)-i]) {
+                continue;
+            }
+            // if at least one character does not coincide is not a palindrome
+            else {
+                return false;
+            }
+        }
+        // Avoid check the string characters twice
+        else {
+            break;
+        }
+    }
+    // is a plindrome
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// pretest
+//console.log(palindromes("Hello., worldd!"));
