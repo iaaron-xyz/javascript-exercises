63138af3926678f9e2126b80ac0c3dab1024decd
Aaron <iaaron.xyz@outlook.com>
2022-10-14 11:46:17 -0500

    finished 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // Start with the neutral value for addition
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  // Start with the neutral value for multiplication
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  // Base case
+	if (n === 0 || n === 1) {
+    return 1;
+  }
+  // For positive values greater than 1
+  let factorial = 1;
+  for (let i = n; i > 1;i--) {
+    factorial *= i; 
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -31,3 +50,5 @@ module.exports = {
   power,
   factorial
 };
+
+//console.log(add([1,2,3]));
